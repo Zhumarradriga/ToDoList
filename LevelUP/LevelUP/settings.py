510cbd08f7ad3234f6dd508d6258842d70fc2b31
Александр Красильников <avk293@tpu.ru>
2025-03-25 18:27:41 +0700

    add models in todo_auth\models.py and create app

--- LevelUP/LevelUP/settings.py
+++ LevelUP/LevelUP/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ToDoAuth.apps.TodoauthConfig'
+    'todo_auth.apps.TodoauthConfig'
 ]
 
 MIDDLEWARE = [
--- LevelUP/todo_auth/apps.py
+++ LevelUP/todo_auth/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TodoauthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ToDoAuth'
+    name = 'todo_auth'
