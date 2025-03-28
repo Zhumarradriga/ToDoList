bd40f714ce828ffc399b93e13a3b577c4b81280c
Александр Красильников <avk293@tpu.ru>
2025-03-28 12:59:26 +0700

    pep8 changes

--- LevelUP/todo_stats/models.py
+++ LevelUP/todo_stats/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
 
+
 # Create your models here.
 class Stat(models.Model):
-    name=models.CharField(max_length=50)
-    is_default=models.BooleanField(default=False)
-    created_by=models.ForeignKey('todo_users.Users',on_delete=models.SET_NULL, null=True, blank=True)
-    color=models.CharField(
+    name = models.CharField(max_length=50)
+    is_default = models.BooleanField(default=False)
+    created_by = models.ForeignKey('todo_users.Users', on_delete=models.SET_NULL, null=True, blank=True)
+    color = models.CharField(
         max_length=7,
         default='#000000',
         validators=[RegexValidator(r'^#[0-9A-Fa-f]{6}$', 'Цвет должен быть в формате HEX (#RRGGBB)')]
     )
-    icon_id=models.PositiveIntegerField(
-        validators=[MinValueValidator(1),MaxValueValidator(50)],
+    icon_id = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(50)],
         null=True,
         blank=True
     )
 
     class Meta:
-        db_table='Stats'
-        constraints=[
+        db_table = 'Stats'
+        constraints = [
             models.UniqueConstraint(fields=['name', 'created_by'], name='unique_stat_name_per_user')
         ]
+
     def __str__(self):
         return self.name
